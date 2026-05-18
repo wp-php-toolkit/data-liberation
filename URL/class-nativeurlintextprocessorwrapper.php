6b730b8cb7ca91ad69f13b1c1c7adf3dfc655c78
Adam Zieliński <adam@adamziel.com>
2026-05-18 16:56:59 +0200

    Fix native wrapper autoload without extension

--- URL/class-nativeurlintextprocessorwrapper.php
+++ URL/class-nativeurlintextprocessorwrapper.php
@@ -1,5 +1,10 @@
 <?php
+// phpcs:disable Generic.Classes.DuplicateClassName.Found,Generic.Files.OneObjectStructurePerFile.MultipleFound
 
 namespace WordPress\DataLiberation\URL;
 
-class NativeURLInTextProcessorWrapper extends NativeURLInTextProcessor {}
+if ( class_exists( 'WordPress\\DataLiberation\\URL\\NativeURLInTextProcessor', false ) ) {
+	class NativeURLInTextProcessorWrapper extends NativeURLInTextProcessor {}
+} else {
+	class NativeURLInTextProcessorWrapper {}
+}
